1ca877640d6642e85d6ec5e8d5efc35f742fcc09
João Vitor <diasjoaovitor1997@gmail.com>
2022-11-22 09:55:39 -0300

    refactor: moves firebase config

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,16 +1,2 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-
-const app = initializeApp({
-  apiKey: import.meta.env.VITE_API_KEY as string,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
-  databaseURL: import.meta.env.VITE_DATABASE_URL as string,
-  projectId: import.meta.env.VITE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_APP_ID as string
-})
-
-export const authConfig = getAuth(app)
-export const db = getFirestore(app)
+export * from './firebase'
+export * from './rschedule'
